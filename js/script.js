d4056671fe982cabd2fdac9557ab1e0311b5a93a
rmergeee <63661366+rmergeee@users.noreply.github.com>
2025-06-29 20:41:46 +0300

    Rewrite the JavaScript code.
    - Completely rewrite the JS code.
    - Implemented functionality:
      - Addition.
      - Subtraction.
      - Multiplication
      - Division.

--- js/script.js
+++ js/script.js
@@ -2,156 +2,136 @@ let firstNumber;
 let secondNumber;
 let operator;
 
-let displayContent;
+const numberButtons = document.querySelector(".numbers");
+const inputField = document.querySelector(".input-numbers");
+const clearButton = document.querySelector(".ac-button");
+const backspace = document.querySelector(".backspace");
+const operatorButtons = document.querySelector(".operation");
+const equelsButton = document.querySelector(".button-equals");
 
-function operate(operator, firstNumber, secondNumber) {
-    if (operator === "+") {
-        console.log(firstNumber, operator, secondNumber);
-        return firstNumber + secondNumber;
-    } else if (operator === "-") {
-        return subtract(firstNumber,secondNumber);
-    } else if (operator === "/") {
-        return divide(firstNumber,secondNumber);
-    } else if (operator === "*") {
-        return multiply(firstNumber,secondNumber);
+numberButtons.addEventListener("click", (event) => {
+    let target = event.target;
+
+    switch (target.className) {
+        case "num-1 calc-button":
+            if (inputField.textContent === "0") inputField.textContent = "";
+            inputField.textContent += 1;
+            break;
+        case "num-2 calc-button":
+            if (inputField.textContent === "0") inputField.textContent = "";
+            inputField.textContent += 2;
+            break;
+        case "num-3 calc-button":
+            if (inputField.textContent === "0") inputField.textContent = "";
+            inputField.textContent += 3;
+            break;
+        case "num-4 calc-button":
+            if (inputField.textContent === "0") inputField.textContent = "";
+            inputField.textContent += 4;
+            break;
+        case "num-5 calc-button":
+            if (inputField.textContent === "0") inputField.textContent = "";
+            inputField.textContent += 5;
+            break;
+        case "num-6 calc-button":
+            if (inputField.textContent === "0") inputField.textContent = "";
+            inputField.textContent += 6;
+            break;
+        case "num-7 calc-button":
+            if (inputField.textContent === "0") inputField.textContent = "";
+            inputField.textContent += 7;
+            break;
+        case "num-8 calc-button":
+            if (inputField.textContent === "0") inputField.textContent = "";
+            inputField.textContent += 8;
+            break;
+        case "num-9 calc-button":
+            if (inputField.textContent === "0") inputField.textContent = "";
+            inputField.textContent += 9;
+            break;
+        case "button-0 calc-button":
+            if (inputField.textContent === "0") inputField.textContent = "";
+            inputField.textContent += 0;
+            break;
+        case "button-dot calc-button":
+            if (inputField.textContent === "") break;
+            if (inputField.textContent.split("").includes(".")) break;
+            if (inputField.textContent === "0") inputField.textContent = "";
+            inputField.textContent += ".";
+            break;
     }
+})
 
-}
+operatorButtons.addEventListener("click", (event) =>{
+    let target = event.target;
 
-function sum(firstNumber,secondNumber) {
-    console.log(firstNumber, operator, secondNumber);
-    return firstNumber + secondNumber;
-}
+    switch (target.className) {
+        case "button-devide calc-button":
+            if (inputField.textContent === "") break;
+            firstNumber = inputField.textContent;
+            inputField.textContent = "";
+            operator = "/"
+            break;
+        case "button-multiply calc-button":
+            if (inputField.textContent === "") break;
+            firstNumber = inputField.textContent;
+            inputField.textContent = "";
+            operator = "*"
+            break;
+        case "button-minus calc-button":
+            if (inputField.textContent === "") break;
+            if (inputField.textContent === "") break;
+            firstNumber = inputField.textContent;
+            inputField.textContent = "";
+            operator = "-"
+            break;
+        case "button-plus calc-button":
+            if (inputField.textContent === "") break;
+            firstNumber = inputField.textContent;
+            inputField.textContent = "";
+            operator = "+"
+            if (inputField.textContent === "") break;
+            break;
+    }
+})
 
-function subtract(firstNumber,secondNumber) {
-    return firstNumber - secondNumber;
-}
-
-function divide(firstNumber,secondNumber) {
-    return firstNumber / secondNumber;
-}
-
-function multiply(firstNumber,secondNumber) {
-    return firstNumber * secondNumber;
-}
-
-//UI
-const buttonDot = document.querySelector(".button-dot");
-const buttonEquels = document.querySelector(".button-equals");
-const plusButton = document.querySelector(".button-plus");
-const minusButton = document.querySelector(".button-minus");
-const devideButton = document.querySelector(".button-devide");
-const multiplyButton = document.querySelector(".button-multiply");
-const clearButton = document.querySelector(".ac-buttom");
-const backButton = document.querySelector(".backspace");
-const numbers = document.querySelector(".numbers");
-const inputNumbers = document.querySelector(".input-numbers");
-const operationButtons = document.querySelector(".operation");
+equelsButton.addEventListener("click", () => {
+    if (inputField.textContent === "") return;
+    if (inputField.textContent === "0") {
+        alert("На ноль не делим!");
+        return;
+    }
+    if (firstNumber === undefined) return;
+    secondNumber = inputField.textContent;
+    inputField.textContent = operate(operator, firstNumber, secondNumber);
+    
+})
 
 clearButton.addEventListener("click", () => {
-    inputNumbers.textContent = "";
+    inputField.textContent = "";
     firstNumber = undefined;
     secondNumber = undefined;
     operator = undefined;
 })
 
-numbers.addEventListener('click', (event) => {
-    let target = event.target;
-    switch(target.className) {
-        case 'button-0 calc-button':
-            if (inputNumbers.textContent === "0") inputNumbers.textContent = "";
-            displayContent = 0;
-            inputNumbers.textContent += displayContent;
-            break;
-        case 'num-1 calc-button':
-            if (inputNumbers.textContent === "0") inputNumbers.textContent = "";
-            displayContent = 1;
-            inputNumbers.textContent += displayContent;
-            break;
-        case 'num-2 calc-button':
-            if (inputNumbers.textContent === "0") inputNumbers.textContent = "";
-            displayContent = 2;
-            inputNumbers.textContent += displayContent;
-            break;
-        case 'num-3 calc-button':
-            if (inputNumbers.textContent === "0") inputNumbers.textContent = "";
-            displayContent = 3;
-            inputNumbers.textContent += displayContent;
-            break;
-        case 'num-4 calc-button':
-            if (inputNumbers.textContent === "0") inputNumbers.textContent = "";
-            displayContent = 4;
-            inputNumbers.textContent += displayContent;
-            break;
-        case 'num-5 calc-button':
-            if (inputNumbers.textContent === "0") inputNumbers.textContent = "";
-            displayContent = 5;
-            inputNumbers.textContent += displayContent;
-            break;
-        case 'num-6 calc-button':
-            if (inputNumbers.textContent === "0") inputNumbers.textContent = "";
-            displayContent = 6;
-            inputNumbers.textContent += displayContent;
-            break;
-        case 'num-7 calc-button':
-            if (inputNumbers.textContent === "0") inputNumbers.textContent = "";
-            displayContent = 7;
-            inputNumbers.textContent += displayContent;
-            break;
-        case 'num-8 calc-button':
-            if (inputNumbers.textContent === "0") inputNumbers.textContent = "";
-            displayContent = 8;
-            inputNumbers.textContent += displayContent;
-            break;
-        case 'num-9 calc-button':
-            if (inputNumbers.textContent === "0") inputNumbers.textContent = "";
-            displayContent = 9;
-            inputNumbers.textContent += displayContent;
-            break;
-        case 'button-dot calc-button':
-            if (inputNumbers.textContent === "" || inputNumbers.textContent === "0") break;
-            inputNumbers.textContent += ".";
-            break;
+backspace.addEventListener("click", () => (
+    inputField.textContent = inputField
+                                    .textContent
+                                    .slice(0, inputField.textContent.length - 1)
+));
+
+function operate(operator, firstNumber, secondNumber) {
+    firstNumber = Number(firstNumber);
+    secondNumber = Number(secondNumber);
+
+    switch (operator) {
+        case "+":
+            return firstNumber + secondNumber;
+        case "-":
+            return firstNumber - secondNumber;
+        case "/":
+            return firstNumber / secondNumber;
+        case "*":
+            return firstNumber * secondNumber;
     }
-});
-
-operationButtons.addEventListener('click', (event) => {
-    let target = event.target;
-    switch(target.className) {
-        case "button-devide calc-button":
-            if (inputNumbers.textContent === "0") break;
-            if (inputNumbers.textContent !== "") {
-                firstNumber = inputNumbers.textContent;
-                operator = "/"
-                inputNumbers.textContent = "";
-            }
-            break;
-        case "button-multiply calc-button":
-            if (inputNumbers.textContent !== "") {
-                firstNumber = inputNumbers.textContent;
-                operator = "*"
-                inputNumbers.textContent = "";
-            }
-            break;
-        case "button-minus calc-buttons":
-            if (inputNumbers.textContent !== "") {
-                firstNumber = inputNumbers.textContent;
-                operator = "-"
-                inputNumbers.textContent = "";
-            }
-            break;
-        case "button-plus calc-button":
-            if (inputNumbers.textContent !== "") {
-                firstNumber = inputNumbers.textContent;
-                operator = "+ "
-                inputNumbers.textContent = "";
-            }
-            break;
-
-    }
-});
-
-buttonEquels.addEventListener("click", () => {
-    secondNumber = inputNumbers.textContent;
-    console.log(operate(operator, firstNumber, secondNumber));
-});
+}
